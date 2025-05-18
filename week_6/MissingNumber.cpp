55cf583e0a912367d3670830ee1e5be32337fc20
siddhit-kale <ksiddhit9@gmail.com>
2025-05-18 13:53:11 +0530

    MissingNumber

---
+++ week_6/MissingNumber.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+uisng namespace std;
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int TotalSum = n * (n + 1) / 2;
+        int currentSum = 0;
+        
+        for (int i : nums) {
+            currentSum += i;
+        }
+        
+        return TotalSum - currentSum;
+    }
+};
