29ffb9180559058e9f68f1656a5d8a1eba128c6c
siddhit-kale <ksiddhit9@gmail.com>
2025-05-20 14:57:42 +0530

    Lexicographically Smallest Palindrome

---
+++ Week_7/Lexicographically Smallest Palindrome.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int i=0; 
+        int j = s.length() - 1;
+        while(i < j) {
+            s[i] = min(s[i] , s[j]);
+            s[j] = s[i];
+            i++, j--;
+        }
+        return s;
+    }
+};
